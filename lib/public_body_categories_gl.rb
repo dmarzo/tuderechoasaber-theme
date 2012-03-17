983b27c4390663651ec82a82eef30da31a512497
David Cabo <david@calibea.com>
2012-03-17 21:41:23 +0100

    Add Galician translations (some)

---
+++ lib/public_body_categories_gl.rb
@@ -0,0 +1,20 @@
+PublicBodyCategories.add(:gl, [
+    "Administración Central",
+        [ "ministerio", "Ministerios", "un Ministerio" ],
+        [ "secretaria_estado", "Secretarías do Estado", "una Secretaría de Estado"],
+        [ "direccion_general", "Direccións Xenerais", "una Dirección General"],
+        [ "agencia_estatal", "Axencias Estatais e Outros Organismos", "una Agencia Estatal u órganismo dependiente"],
+        [ "embajada", "Embaixadas", "una Embajada Española"],
+        [ "organo_constitucional", "Órganos Constitucionais", "un órgano constitucional"],
+        [ "organo_regulador", "Organismos Reguladores", "un organismo regulador"],
+        [ "empresa_publica", "Empresas Públicas", "una empresa pública"],
+        [ "fundacion_publica", "Fundacións Públicas", "una fundación pública"],
+        [ "universidad", "Universidades", "una universidad"],
+        [ "partido_politico", "Partidos Políticos", "un partido político"],
+    "Administración Autonómica",
+        [ "ca", "Gobiernos Autonómicos", "un gobierno autonómico"],
+        [ "asamblea_ca", "Asambleas Autonómicas", "una asamblea autonómica"],
+        [ "delegacion_gobierno", "Delegacions do Goberno", "una delegación del gobierno"],
+    "Administración Local",
+        [ "ayuntamiento", "Concello", "un ayuntamiento"]
+])
