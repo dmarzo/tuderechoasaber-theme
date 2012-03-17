e0fffda0c68136a0d94073e487bbcec900e6ce39
David Cabo <david@calibea.com>
2012-03-17 19:14:50 +0100

    Added translations to Basque. Part of them.

---
+++ lib/public_body_categories_eu.rb
@@ -0,0 +1,20 @@
+PublicBodyCategories.add(:eu, [
+    "Administrazio Zentrala",
+        [ "ministerio", "Ministerioak", "un Ministerio" ],
+        [ "secretaria_estado", "Estatu Idazkaritzak", "una Secretaría de Estado"],
+        [ "direccion_general", "Zuzendaritza Nagusiak", "una Dirección General"],
+        [ "agencia_estatal", "Estatu Agentziak eta beste Erakundeak", "una Agencia Estatal u órganismo dependiente"],
+        [ "embajada", "Enbaxadak", "una Embajada Española"],
+        [ "organo_constitucional", "Organo Konstituzionalak", "un órgano constitucional"],
+        [ "organo_regulador", "Erakunde Arautzaileak", "un organismo regulador"],
+        [ "empresa_publica", "Enpresa Publikoak", "una empresa pública"],
+        [ "fundacion_publica", "Fundazio Publikoak", "una fundación pública"],
+        [ "universidad", "Unibertsitateak", "una universidad"],
+        [ "partido_politico", "Alderdi Politikoak", "un partido político"],
+    "Administrazio Autonomikoa",
+        [ "ca", "Gobernu Autonomikoa", "un gobierno autonómico"],
+        [ "asamblea_ca", "Biltzar Autonomikoak", "una asamblea autonómica"],
+        [ "delegacion_gobierno", "Gobernuko Ordezkaritzak", "una delegación del gobierno"],
+    "Administrazio Lokala",
+        [ "ayuntamiento", "Udalak", "un ayuntamiento"]
+])
